f00374d5a8369f0ca975c8ba248342df4e0ab95d
ujangpudin <ujangpudin04@gmail.com>
2023-09-07 08:00:31 +0700

    up

--- main.go
+++ main.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 )
 
+func isPrime(num int) bool {
+    if num <= 1 {
+        return false
+    }
+    for i := 2; i*i <= num; i++ {
+        if num%i == 0 {
+            return false
+        }
+    }
+    return true
+}
 
 func main() {
     fmt.Println("Bilangan Prima:")
     for i := 2; i <= 100; i++ {
-        if IsPrime(i) {
+        if isPrime(i) {
             fmt.Println(i)
         }
     }
